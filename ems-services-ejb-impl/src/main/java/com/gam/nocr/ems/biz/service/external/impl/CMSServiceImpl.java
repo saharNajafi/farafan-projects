7df2390d80cfa8561dcfd86a8a608518c2740f4c
safiary <hasan_safiary@yahoo.com>
2017-12-23 09:20:55 +0330

    change user site ispostneeded to 1 or 2

--- ems-services-ejb-impl/src/main/java/com/gam/nocr/ems/biz/service/external/impl/CMSServiceImpl.java
+++ ems-services-ejb-impl/src/main/java/com/gam/nocr/ems/biz/service/external/impl/CMSServiceImpl.java
@@ -289,7 +289,7 @@ public class CMSServiceImpl extends AbstractService implements CMSServiceLocal, 
                 userSiteInfo.setUserSiteID(String.valueOf(dbEnrollmentOffice.getId()));
                 userSiteInfo.setUserSiteCode(dbEnrollmentOffice.getCode());
                 userSiteInfo.setUserSiteName(dbEnrollmentOffice.getName());
-                userSiteInfo.setIsPostNeeded(dbEnrollmentOffice.getIsPostNeeded() ? 1 : 0);
+                userSiteInfo.setIsPostNeeded(dbEnrollmentOffice.getIsPostNeeded() ? 1 : 2);
                 userSiteInfo.setPostDestinationCode(dbEnrollmentOffice.getPostDestinationCode()!= null ? dbEnrollmentOffice.getPostDestinationCode():"");
                 String userSiteContact = dbEnrollmentOffice.getAddress() + ", " +
                         dbEnrollmentOffice.getPhone() + ", " +
