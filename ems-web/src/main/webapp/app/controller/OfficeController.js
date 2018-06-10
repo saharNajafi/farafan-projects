59e3f59218d5b6a7bc27a410fb32b52568fb9aec
m.hoseini <mh_1977_654@yahoo.com>
2018-06-10 10:34:33 +0430

    add comment

--- ems-web/src/main/webapp/app/controller/OfficeController.js
+++ ems-web/src/main/webapp/app/controller/OfficeController.js
@@ -148,6 +148,7 @@ Ext.define('Ems.controller.OfficeController', {
         	       click: function(sender) {
                        var form = sender.up('dialog');
                        var list = [];
+                       ///Add CapacityOfficeId
                        var obj = { enrollmentOfficeID: form.enrollmentOfficeID, officeCapacityID: null };
                        var me = this;
                         Ext.each(form.query('field'),
