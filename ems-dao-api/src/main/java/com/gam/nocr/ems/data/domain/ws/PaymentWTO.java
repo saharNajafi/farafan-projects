66e50c2049ac4878cbfcfaec9a769a5e16c11a9e
safiary <hasan_safiary@yahoo.com>
2018-08-19 11:03:43 +0430

    change in payment

--- ems-dao-api/src/main/java/com/gam/nocr/ems/data/domain/ws/PaymentWTO.java
+++ ems-dao-api/src/main/java/com/gam/nocr/ems/data/domain/ws/PaymentWTO.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 /**
  * Created by Najafi Sahar najafisahaar@yahoo.com on 1/6/18.
  */
-public class PaymentWTO extends SecureWTO implements Serializable {
+public class PaymentWTO implements Serializable {
     private long preRegistrationId;
     private RegistrationPaymentWTO registrationPaymentWTO;
     private String nationalId;
