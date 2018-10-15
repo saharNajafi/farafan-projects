75fe11e5ca14cf8a9b072e2bbcd4632607569909
hasan <safyari@hasan.com>
2018-10-15 14:51:10 +0330

    sql change

--- sql/ems-2.5.5.sql
+++ sql/ems-2.5.5.sql
@@ -8,3 +8,6 @@ INSERT INTO "COMM_DEV_EMS"."INFT_SYSTEM_PROFILE" (SPF_ID, SPF_PRF_ID, SPF_CLOB) 
 INSERT INTO "COMM_DEV_EMS"."INFT_PROFILE_KEY" (PRF_ID, PRF_PARENT_ID, PRF_SYS_ID, PRF_NAME, PRF_PRIORITY_CHAIN, PRF_STATUS)
 VALUES ('2191', '3', '1', 'nocr.ems.profile.fingNormal1SizeKB', 'P,D,S', 'H');
 INSERT INTO "COMM_DEV_EMS"."INFT_SYSTEM_PROFILE" (SPF_ID, SPF_PRF_ID, SPF_CLOB) VALUES ('2191', '2191', '2');
+
+
+the featureExtractid and featureextractversion for all office setting must be filling.
