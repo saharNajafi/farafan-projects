a8cbc6c6a846758abde1c8b57e301657cc2b57a4
hasan <safyari@hasan.com>
2018-10-15 09:49:10 +0330

    default value for feature extract id

--- ems-dao-api/src/main/java/com/gam/nocr/ems/data/domain/BiometricInfoTO.java
+++ ems-dao-api/src/main/java/com/gam/nocr/ems/data/domain/BiometricInfoTO.java
@@ -30,7 +30,7 @@ public class BiometricInfoTO extends ExtEntityTO  {
     private MinutiaType minType;
     private CitizenTO citizen;
     private FingerQualityType fingerQualityType = FingerQualityType.UNDEFINED;
-    private String featureExtractorID = "0001";
+    private String featureExtractorID = "0201";
     
 
     @Id
@@ -91,7 +91,7 @@ public class BiometricInfoTO extends ExtEntityTO  {
 		this.fingerQualityType = fingerQualityType;
 	}
 
-    @Column(name = "BMI_FEATURE_EXTRACTOR_ID",nullable = false,length = 4, columnDefinition = "varchar2(4) default '0001'")
+    @Column(name = "BMI_FEATURE_EXTRACTOR_ID",nullable = false,length = 4, columnDefinition = "varchar2(4) default '0201'")
     public String getFeatureExtractorID() {
         return featureExtractorID;
     }
