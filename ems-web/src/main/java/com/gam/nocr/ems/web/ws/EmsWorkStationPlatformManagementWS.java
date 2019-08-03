74e015d5cf6e4a12df873c4b01d17010eb5019a8
mazaher.namjoofar <mazaher.namjoofar@farafan.com>
2019-08-03 14:24:06 +0430

    EMS-867===> change method name of workstation info

--- ems-web/src/main/java/com/gam/nocr/ems/web/ws/EmsWorkStationPlatformManagementWS.java
+++ ems-web/src/main/java/com/gam/nocr/ems/web/ws/EmsWorkStationPlatformManagementWS.java
@@ -7,6 +7,7 @@ import com.gam.nocr.ems.biz.delegator.WorkstationInfoDelegator;
 import com.gam.nocr.ems.biz.delegator.WorkstationPluginsDelegator;
 import com.gam.nocr.ems.config.WebExceptionCode;
 import com.gam.nocr.ems.data.domain.WorkstationInfoTO;
+import com.gam.nocr.ems.data.domain.WorkstationPluginsTO;
 import com.gam.nocr.ems.data.domain.ws.*;
 import com.gam.nocr.ems.util.EmsUtil;
 import com.gam.nocr.ems.util.Utils;
@@ -17,6 +18,7 @@ import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.ws.WebFault;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -34,7 +36,7 @@ public class EmsWorkStationPlatformManagementWS extends EMSWS {
     private WorkstationPluginsDelegator workstationPluginsDelegator = new WorkstationPluginsDelegator();
 
     @WebMethod
-    public boolean isReliableVerInquiryRequired(
+    public boolean isWorkstationInfoRequired(
             @WebParam(name = "securityContextWTO") SecurityContextWTO securityContextWTO,
             @WebParam(name = "WorkstationCode", targetNamespace = "")
             @XmlElement(required = true, nillable = false) String workstationCode
@@ -44,7 +46,7 @@ public class EmsWorkStationPlatformManagementWS extends EMSWS {
     }
 
     @WebMethod
-    public String getReliableVerByPlatform(
+    public String registerWorkstationInfo(
             @WebParam(name = "securityContextWTO") SecurityContextWTO securityContextWTO,
             @WebParam(name = "WorkstationCode", targetNamespace = "")
             @XmlElement(required = true, nillable = false) String workstationCode,
@@ -61,6 +63,35 @@ public class EmsWorkStationPlatformManagementWS extends EMSWS {
         }
         return verCode;
     }
+
+    @WebMethod
+    public String registerWorkstationPlugins(
+            @WebParam(name = "securityContextWTO") SecurityContextWTO securityContextWTO,
+            @WebParam(name = "WorkstationCode", targetNamespace = "")
+            @XmlElement(required = true, nillable = false) String workstationCode,
+            @WebParam(name = "PluginInfo", targetNamespace = "")
+            @XmlElement(required = true, nillable = false) List<PluginInfoWTO> pluginInfoList
+    ) throws InternalException {
+        UserProfileTO userProfileTO = super.validateRequest(securityContextWTO);
+        String verCode = null;
+        List<WorkstationPluginsTO> workstationPluginsList = new ArrayList<WorkstationPluginsTO>();
+        try {
+            if (pluginInfoList.size() > 0) {
+                for (PluginInfoWTO pluginInfo : pluginInfoList) {
+                    WorkstationPluginsTO workstationPlugins = new WorkstationPluginsTO();
+                    workstationPlugins.setPluginName(pluginInfo.getPluginName());
+                    workstationPlugins.setState(Short.valueOf(pluginInfo.getState()));
+                    workstationPluginsList.add(workstationPlugins);
+                }
+            }
+            verCode = workstationPluginsDelegator.getReliableVerByPlugin(
+                    userProfileTO, workstationCode, workstationPluginsList);
+        } catch (BaseException e) {
+            throw new InternalException(e.getExceptionCode(), new EMSWebServiceFault(e.getMessage()), e);
+        }
+        return verCode;
+    }
+
 
     @WebMethod
     public List<String> getCompatibleClientVerList(
